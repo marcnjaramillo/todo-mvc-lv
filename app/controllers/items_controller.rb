6543772b9332066a4354b086f3673f6e2d2a0b26
marcnjaramillo <marcn.jaramillo@gmail.com>
2016-12-05 00:20:47 -0800

    Finished

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,31 @@
+class ItemsController < ApplicationController
+  def create
+    @list = List.find(params[:list_id]) # finding the parent
+    @item = @list.items.build(item_params)
+    if @item.save
+      redirect_to list_path(@list)
+    else
+      render "lists/show"
+    end
+  end
+
+  # PATCH - /lists/:list_id/items/:id
+  def update
+    @item = Item.find(params[:id])
+    @item.update(item_params)
+
+    redirect_to list_path(@item.list)
+  end
+
+  def destroy
+    @item = Item.find(params[:id])
+    @item.destroy
+    redirect_to list_path(@item.list)
+  end
+
+  private
+    def item_params
+      params.require(:item).permit(:description, :status)
+    end
+
+end
--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -3,23 +3,25 @@ class ListsController < ApplicationController
   def index
     @list = List.new
     @lists = List.all
-    # render 'lists/index.html.erb'
   end
 
   def show
     @list = List.find(params[:id])
+    @item = Item.new
   end
 
   def create
     @list = List.new(list_params)
-    @list.name = params[:list][:name]
-    @list.save
-
-    redirect_to list_url(@list)
+    if @list.save
+      redirect_to list_url(@list)
+    else
+      @lists = List.all
+      render :index
+    end
   end
   private
 
-    def list_params # strong parameters
+    def list_params
       params.require(:list).permit(:name)
     end
 end
